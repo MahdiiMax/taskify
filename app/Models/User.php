de1210d52e43d2e2c8c9f68dec9543ed70f5ccf0
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-03 22:04:33 +0330

    feat: add task & user resources

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Http\Resources\V1\UserResource;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Attributes\UseResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -13,6 +16,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
+#[UseResource(UserResource::class)]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
