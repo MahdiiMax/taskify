01b6a75ca27c9dd39f8541bf8467cbb17f605805
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-04 02:08:51 +0330

    feat: add task factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +18,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Task::factory(15)->create([
+        //     "user_id" => User::inRandomOrder()->first()->id
+        // ]);
+
+        User::factory(10)->hasTasks(5)->create();
     }
 }
