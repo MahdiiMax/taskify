0eccf00ca84256e413625068237f5ee8b10e95a1
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-06 23:12:02 +0330

    feat: move resources to Http/Resources/Api/V1 and add task filtering/search

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Http\Resources\V1\TaskResource;
+use App\Http\Resources\Api\V1\TaskResource;
 use Illuminate\Database\Eloquent\Attributes\UseResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -15,15 +15,15 @@ class Task extends Model
     use HasFactory,SoftDeletes;
 
     protected $fillable = [
-        "title",
-        "description",
-        "status",
-        "priority",
-        "user_id",
-        "due_date"
+        'title',
+        'description',
+        'status',
+        'priority',
+        'user_id',
+        'due_date',
     ];
 
-    public function user(): BelongsTo 
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use App\Http\Resources\V1\UserResource;
+use App\Http\Resources\Api\V1\UserResource;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -20,7 +20,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * Get the attributes that should be cast.
